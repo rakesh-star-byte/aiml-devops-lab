e26247624b290105572a93046a6ae0d85999462b
Rakesh <245123748049@mvsrec.edu.in>
2026-04-10 13:14:15 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,11 @@
+# Small program to demonstrate operators
+
+# Take input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
